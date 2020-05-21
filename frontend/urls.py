92831300f5e083edebaa89e488ee0e60b4dfe259
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-05-21 14:31:20 +0800

    add robots.txt

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+from django.urls import path, re_path
+from django.views.generic.base import TemplateView
 from . import views
 
 
 urlpatterns = [
+    path('robots.txt', TemplateView.as_view(template_name='frontend/robots.txt', content_type='text/plain')),
     re_path(r'^.*/?$', views.index),
 ]
