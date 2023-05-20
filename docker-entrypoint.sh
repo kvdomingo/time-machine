fd9ab35dadce32bf7f0cd8b0142f784a2c1bfa01
Kenneth V. Domingo <hello@kvdomingo.xyz>
2023-05-20 22:10:09 +0800

    fix: lab deployment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,12 @@
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
 
-if [ "$PYTHON_ENV" != "development" ]; then
+if [ "$PYTHON_ENV" = "development" ]; then
+  exec gunicorn --bind 0.0.0.0:5000 --config ./gunicorn.conf.py --reload
+elif [ "$PYTHON_ENV" = "lab" ]; then
   python manage.py collectstatic --noinput
-  reload_flag=""
+  exec gunicorn --bind unix:/var/run/gunicorn.sock --config ./gunicorn.conf.py
 else
-  reload_flag="--reload"
+  python manage.py collectstatic --noinput
+  exec gunicorn --bind 0.0.0.0:5000 --config ./gunicorn.conf.py
 fi
-
-exec gunicorn --bind 0.0.0.0:5000 --config ./gunicorn.conf.py $reload_flag
