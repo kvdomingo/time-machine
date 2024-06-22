4a97c98ef2227a6c114954e442640b55b1de6429
Kenneth V. Domingo <hello@kvd.studio>
2024-06-22 22:50:32 +0800

    feat(ui): add catppuccin theme

--- ui/tailwind.config.cjs
+++ ui/tailwind.config.cjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
-	darkMode: ["class"],
-	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  darkMode: ["class"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {},
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      defaultFlavour: "mocha",
+      prefix: false,
+    }),
+  ],
 };
